44a6f6be2f71f8061e934a76fff102967e507fc6
Frank Kevin Zey <frank.kevin.zey@outlook.com>
2024-10-13 00:33:01 +0200

    project adjustments

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,7 @@ project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
+	staticruntime "Off"
 
 	targetdir (BIN .. "/%{prj.name}")
 	objdir (OBJ .. "/%{prj.name}")
@@ -19,14 +20,17 @@ project "yaml-cpp"
 		"include"
 	}
 
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE",
+	}
+
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
