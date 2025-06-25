54a264631316635696a0397a0693cf2c676a276e
Frank Kevin Zey <frank.kevin.zey@outlook.com>
2025-06-25 23:17:02 +0200

    premake script adjusted

--- premake5.lua
+++ premake5.lua
@@ -2,6 +2,10 @@ project "yaml-cpp"
 	kind "StaticLib"
 	language "C++"
 	cppdialect "C++20"
+	staticruntime "off"
+	exceptionhandling "off"
+
+	characterset "Unicode"
 
 	targetdir (BIN .. "/%{prj.name}")
 	objdir (OBJ .. "/%{prj.name}")
@@ -19,19 +23,25 @@ project "yaml-cpp"
 		"include"
 	}
 
+	defines
+	{
+		'YAML_CPP_STATIC_DEFINE'
+	}
+
 	filter "system:windows"
 		systemversion "latest"
-		staticruntime "On"
 
 	filter "system:linux"
-		pic "On"
 		systemversion "latest"
-		staticruntime "On"
 
 	filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
 	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
 		runtime "Release"
 		optimize "on"
